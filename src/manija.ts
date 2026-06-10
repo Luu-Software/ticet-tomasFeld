befcf7fb53dd6d90a12090aa4cdf803f90d85d07
Tomas Feld <96779650+tomasFeld@users.noreply.github.com>
2026-06-10 14:04:01 -0300

    MANIJA

--- src/manija.ts
+++ src/manija.ts
@@ -1,5 +1,10 @@
 export function generarLorem(n: number): string {
-  // COMPLETAR: Implementar la función generarLorem que reciba un número n y devuelva una cadena de texto con la frase "Lorem Ipsum" repetida n veces, separada por espacios. Por ejemplo, si n es 3, la función debería devolver "Lorem Ipsum Lorem Ipsum Lorem Ipsum".
+
+  if (n <= 0) {
+    return '.';
+  }
+
+  return "Lorem Ipsum " + generarLorem(n - 1);
 }
 
-console.log(generarLorem(5));
+console.log(generarLorem(5)); 
